5176e3f6114f4139599423f54598230c4298dbd0
Daniil Filinyuk <orstred13@gmail.com>
2025-03-01 16:07:26 +0700

    Добавлено сворачивание в трей при запуске и при закрытии окна

--- OwlControlCenter/App.xaml.cs
+++ OwlControlCenter/App.xaml.cs
@@ -1,10 +1,57 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Reflection;
 using System.Windows;
+using Application = System.Windows.Application;
 
 namespace OwlControlCenter;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application { }
+public partial class App : Application {
+    private NotifyIcon notifyIcon;
+
+    public void App_Startup(object sender, StartupEventArgs e) {
+        var assembly = Assembly.GetExecutingAssembly();
+        Icon icon;
+        using (var stream = assembly.GetManifestResourceStream("OwlControlCenter.Resources.icon.ico")) {
+            icon = new Icon(stream);
+        }
+
+        notifyIcon = new NotifyIcon {
+            Icon = icon,
+            Visible = true,
+            Text = "OwlControlCenter"
+        };
+        
+        notifyIcon.ContextMenuStrip = CreateContextMenu();
+        
+        Current.MainWindow = new MainWindow();
+        Current.MainWindow.Hide();
+
+        notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
+    }
+
+    private ContextMenuStrip CreateContextMenu() {
+        var contextMenu = new ContextMenuStrip();
+        
+        var exitMenuItem = new ToolStripMenuItem("Закрыть", null, ExitMenuItem_Click);
+        contextMenu.Items.Add(exitMenuItem);
+
+        return contextMenu;
+    }
+
+    private void ExitMenuItem_Click(object sender, EventArgs e) {
+        notifyIcon.Dispose();
+        Current.Shutdown();
+    }
+
+    private void NotifyIcon_MouseDoubleClick(object? sender, System.Windows.Forms.MouseEventArgs e) {
+        if (Current.MainWindow.Visibility != Visibility.Visible) {
+            Current.MainWindow.Show();
+        } else {
+            Current.MainWindow.Hide();
+        }
+    }
+}
--- OwlControlCenter/MainWindow.xaml.cs
+++ OwlControlCenter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,5 +18,10 @@ namespace OwlControlCenter;
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+    
+    private void MainWindow_OnClosing(object? sender, CancelEventArgs e) {
+        e.Cancel = true;
+        Hide();
     }
 }
