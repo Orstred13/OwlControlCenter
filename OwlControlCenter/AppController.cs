b3ed3c6a2e9a2f6d56690f3afab8464dc28a9896
Daniil Filinyuk <orstred13@gmail.com>
2025-03-10 12:22:04 +0700

    Добавлен метод для изменения громкости

--- OwlControlCenter/AppController.cs
+++ OwlControlCenter/AppController.cs
@@ -1,29 +1,36 @@
 ﻿using System.Diagnostics;
 using System.Windows.Shapes;
+using NAudio.CoreAudioApi;
 
 namespace OwlControlCenter;
 
 public class AppController {
-    private Process _process;
+    private Process[] _processes {
+        get => GetProcessByName(processName);
+    }
+
     public string AppPath { get; }
 
     private string processName {
-        get => System.IO.Path.GetFileName(AppPath);
+        get => System.IO.Path.GetFileNameWithoutExtension(AppPath);
     }
 
     public AppController(string appPath) {
         AppPath = appPath;
     }
 
-
-    // Запуск приложения
-    public void StartApp() {
-        if (_process != null && !_process.HasExited) {
-            return;
+    public Process[] GetProcessByName(string processName) {
+        Process[] processes = Process.GetProcessesByName(processName);
+        if (processes.Length > 0) {
+            return processes;
+        } else {
+            return null;
         }
+    }
 
+    public void StartApp() {
         try {
-            _process = Process.Start(AppPath);
+            Process.Start(AppPath);
         } catch (Exception ex) {
             MessageBox.Show($"Ошибка при запуске приложения {processName}", "Ошибка запуска",
                 MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -31,16 +38,37 @@ public class AppController {
     }
 
     public void CloseApp() {
-        if (_process == null || _process.HasExited) {
+        try {
+            foreach (var _process in _processes) {
+                _process.CloseMainWindow();
+                _process.Close();
+            }
+        } catch (Exception ex) {
+            MessageBox.Show($"Ошибка при закрытии приложения {processName}", "Ошибка запуска",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    public void SetAppVolume(float volume) {
+        if (volume < 0.0f || volume > 1.0f) {
             return;
         }
 
         try {
-            _process.CloseMainWindow();
-            _process.Close();
+            var enumerator = new MMDeviceEnumerator();
+            var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            
+            for (int i = 0; i < device.AudioSessionManager.Sessions.Count; i++) {
+                foreach (var _process in _processes) {
+                    var session = device.AudioSessionManager.Sessions[i];
+                    if (session.GetProcessID == _process.Id) {
+                        session.SimpleAudioVolume.Volume = volume;
+                        return;
+                    }
+                }
+            }
         } catch (Exception ex) {
-            MessageBox.Show($"Ошибка при закрытии приложения {processName}", "Ошибка запуска",
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Console.WriteLine($"Ошибка при установке громкости: {ex.Message}");
         }
     }
 }
